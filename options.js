c0df6cdf6284c58afdb8da8410c308b2244753a4
RoniRamon <roniramon260@gmail.com>
2018-04-24 10:54:32 -0700

    add function to add and remove urls from the blockedUrl array
    Co-authored-by: tsai810417 <tsai_cheng@yahoo.com>
    Co-authored-by: roniRamon <roniramon260@gmail.com>

--- options.js
+++ options.js
@@ -12,4 +12,39 @@ function saveTime(){
       document.getElementById('time').innerHTML = "";
     });
   }
+
+  // chrome.storage.sync.get(["blockedUrls"], function(res) {
+    // let blockedUrlsArr = res["blockedUrls"] ? res["blockedUrls"] : [];
+    // console.log(res.blockedUrlsArr);
+    // blockedUrlsArr.push("google.com");
+    // blockedUrlsArr.push("google22.com");
+    // chrome.storage.sync.set({"blockedUrls" : blockedUrlsArr}, console.log( "array updated"));
+  // });
+};
+
+function addUrl(url){
+  chrome.storage.sync.get(["blockUrls"], result => {
+    let blockedUrlsArr = result["blockUrls"]?result["blockUrls"]:[];
+    console.log(blockedUrlsArr);
+    if (!blockedUrlsArr.includes(url)){
+      blockedUrlsArr.push(url);
+      chrome.storage.sync.set({"blockUrls" : blockedUrlsArr}, console.log( "array updated"));
+    } else {
+      console.log("url already in storage");
+    }
+  });
+}
+
+function removeUrl(url){
+  chrome.storage.sync.get(["blockUrls"], result => {
+    let blockedUrlsArr = result["blockUrls"]?result["blockUrls"]:[];
+    let idx = blockedUrlsArr.indexOf(url);
+    if (idx !== -1){
+      blockedUrlsArr.splice(idx,1);
+      chrome.storage.sync.set({"blockUrls" : blockedUrlsArr}, console.log( "array updated"));
+    } else {
+      console.log("url is not in storage");
+    }
+  });
+
 }
