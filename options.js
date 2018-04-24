f500d5ba992b7177dee45dbcb1c82e1929e2ff83
RoniRamon <roniramon260@gmail.com>
2018-04-24 11:46:48 -0700

    show ul child ele for url and keywords
    Co-authored-by: tsai810417 <tsai_cheng@yahoo.com>
    Co-authored-by: roniRamon <roniramon260@gmail.com>

--- options.js
+++ options.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  displayInList('urls');
+  displayInList('keywords');
+
+
   let timeInput = document.getElementById('set-timer');
   timeInput.onclick = () => saveTime();
 
@@ -36,7 +40,7 @@ function add(type, content){
     if (!arr.includes(content)){
       arr.push(content);
       console.log(arr);
-      chrome.storage.sync.set({[type]: arr}, console.log( "array updated"));
+      chrome.storage.sync.set({[type]: arr}, addToUl(type, content));
     } else {
       console.log(`${content} already in storage`);
     }
@@ -49,9 +53,47 @@ function remove(type, content){
     let idx = arr.indexOf(content);
     if (idx !== -1){
       arr.splice(idx,1);
-      chrome.storage.sync.set({[type]: arr}, console.log( "array updated"));
+      chrome.storage.sync.set({[type]: arr}, deleteFromUl(type, idx));
     } else {
       console.log(`${content} is not in storage`);
     }
   });
+}
+
+function displayInList(type) {
+    chrome.storage.sync.get([type], result => {
+      if (type === 'urls'){
+        let ulBlock = document.getElementById('ul-block-url');
+        let li;
+        result[type].forEach( url => {
+          li = document.createElement('li');
+          li.innerHTML = url;
+          ulBlock.appendChild(li);
+        });
+      }
+      else if (type === 'keywords') {
+        let ulBlock = document.getElementById('ul-block-word');
+        let li;
+        result[type].forEach( word => {
+          li = document.createElement('li');
+          li.innerHTML = word;
+          ulBlock.appendChild(li);
+        });
+      }
+    });
+}
+
+function addToUl(type, content) {
+  let ul = type === 'urls'? document.getElementById('ul-block-url') : document.getElementById('ul-block-word');
+  let li = document.createElement('li');
+
+  li.innerHTML = content;
+  ul.appendChild(li);
+
+}
+
+function deleteFromUl(type, idx) {
+  let ul = type === 'urls'? document.getElementById('ul-block-url') : document.getElementById('ul-block-word');
+  ul.removeChild(ul.childNodes[idx+1]);
+
 }
