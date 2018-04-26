0b990d0a102799937e0c16a42e57aece8d57296f
RoniRamon <roniramon260@gmail.com>
2018-04-26 10:29:05 -0700

    working on check for url and redirect

--- eventPage.js
+++ eventPage.js
@@ -1,5 +1,20 @@
 
+
 chrome.tabs.onActivated.addListener(function(tabId, changeInfo) {
+  let arrRes;
+  chrome.tabs.query({currentWindow: true, active: true}, tab => {
+    let activeTabUrl = tab[0].url;
+    activeTabUrl = activeTabUrl.split('/');
+    activeTabUrl = `${activeTabUrl[0]}//${activeTabUrl[2]}`;
+    //get all blocked url from chrome storge
+    chrome.storage.sync.get(['urls'], res => {
+      arrRes = Array.from(res.urls);
+      if (arrRes.includes(activeTabUrl)){
+        
+      }
+    });
+  });
+
     chrome.tabs.executeScript( tabId.tabId, {file: 'content.js'} );
 });
 
