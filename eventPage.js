c096f31393322dcb69f1ce5dced2e6bc64f99e7f
RoniRamon <roniramon260@gmail.com>
2018-04-26 09:51:56 -0700

    fix issue with errors in console log

--- eventPage.js
+++ eventPage.js
@@ -1,9 +1,9 @@
 
 chrome.tabs.onActivated.addListener(function(tabId, changeInfo) {
-    chrome.tabs.executeScript( tabId, {file: 'content.js'} );
+    chrome.tabs.executeScript( tabId.tabId, {file: 'content.js'} );
 });
 
 
 chrome.tabs.onCreated.addListener(function(tabId, changeInfo) {
-    chrome.tabs.executeScript( tabId, {file: 'content.js'} );
+    chrome.tabs.executeScript( tabId.tabId, {file: 'content.js'} );
 });
