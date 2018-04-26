6890249fd71a460e0add608a65c0e3c7e726f5f3
RoniRamon <roniramon260@gmail.com>
2018-04-25 21:27:22 -0700

    added a listener to tab to executeScript content page
    Co-authored-by: tsai810417 <tsai_cheng@yahoo.com>
    Co-authored-by: roniRamon <roniramon260@gmail.com>

---
+++ content.js
@@ -0,0 +1,9 @@
+
+
+document.querySelectorAll('p,span,a').forEach(el => {
+
+
+  // el.innerHTML.includes("google") || el.innerHTML.includes("Google")?
+  // el.parentNode.parentNode.remove() :
+   "";
+});
---
+++ eventPage.js
@@ -0,0 +1,23 @@
+let createdTabs = {};
+
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.create( {}, function( tab ) {
+     createdTabs[tab.id] = true;
+  });
+});
+
+chrome.tabs.onUpdate.addListener(function(tabId, changeInfo) {
+  if( createdTabs[tabId] && changeInfo.url )
+    chrome.tabs.executeScript( tabId, {file: 'content.js'} );
+});
+
+chrome.tabs.onActivated.addListener(function(tabId, changeInfo) {
+  if( createdTabs[tabId] && changeInfo.url )
+    chrome.tabs.executeScript( tabId, {file: 'content.js'} );
+});
+
+
+chrome.tabs.onCreated.addListener(function(tabId, changeInfo) {
+  if( createdTabs[tabId] && changeInfo.url )
+    chrome.tabs.executeScript( tabId, {file: 'content.js'} );
+});
