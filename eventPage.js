c39a0f8dc1ece72151a6a30dd9f852084981bafb
Roni Ramon <roniramon260@gmail.com>
2018-04-27 11:07:49 -0700

    Revert "add redirect when time is not in frame"

--- eventPage.js
+++ eventPage.js
@@ -6,14 +6,8 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
     activeTabUrl = `${activeTabUrl[0]}//${activeTabUrl[2]}`;
     //get all blocked url from chrome storge
     chrome.storage.sync.get(['urls', 'time'], res => {
-      let startHour = res[time][0];
-      let endHour = res[time][1];
-      let currentTime = new Date();
-      let timeStr = `${currentTime.getHours()}${currentTime.getMinutes()}`
-      if (timeStr < (startHour + '00') || timeStr > (endHour + '00')) {
-        document.location = chrome.runtime.getURL('404.html');
-      }
-
+      let startHour = res['time'][0];
+      let endHour = res['time'][1];
       arrRes = Array.from(res.urls);
       if (arrRes.join('').includes(activeTabUrl)){
         chrome.tabs.update(tabId.tabId, {url: chrome.runtime.getURL("404.html") });
