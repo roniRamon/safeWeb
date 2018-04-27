f4cefce0294f44f8ebd1f47e3d9cc22669a4afd2
RoniRamon <roniramon260@gmail.com>
2018-04-27 10:54:06 -0700

    fix issue with fillter on keywords

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
-let arrRes2 = [];
-console.log('test');
+
 chrome.storage.sync.get(['keywords'], res => {
-  arrRes2.push(Array.from(res.keywords));
+  let arrRes2 = Array.from(res.keywords);
   document.querySelectorAll('p,span,a').forEach(el => {
     arrRes2.forEach( word => {
       if(el.innerHTML.toLowerCase().includes(word)){
