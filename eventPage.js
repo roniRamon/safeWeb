b3a07a43f43d989c083a39343597ba5b6891bdde
RoniRamon <roniramon260@gmail.com>
2018-04-27 10:06:28 -0700

    added remove btn to lists container

--- eventPage.js
+++ eventPage.js
@@ -1,15 +1,16 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
   let arrRes;
   chrome.tabs.query({currentWindow: true, active: true}, tab => {
+
     let activeTabUrl = tab[0].url;
     activeTabUrl = activeTabUrl.split('/');
     activeTabUrl = `${activeTabUrl[0]}//${activeTabUrl[2]}`;
     //get all blocked url from chrome storge
     chrome.storage.sync.get(['urls', 'time'], res => {
-      let startHour = res[time][0];
-      let endHour = res[time][1];
+      let startHour = res['time'][0];
+      let endHour = res['time'][1];
       arrRes = Array.from(res.urls);
-      if (arrRes.includes(activeTabUrl)){
+      if (arrRes.join('').includes(activeTabUrl)){
         chrome.tabs.update(tabId.tabId, {url: chrome.runtime.getURL("404.html") });
       }
     });
